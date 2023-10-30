658a7bd1f82166455952a176e575064c6bd17c6c
DarkPalad1n <seitz.pascal@t-online.de>
2023-10-30 14:45:49 +0100

    created flask api endpoint

---
+++ FlaskAPI/wsgi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 20:34:50 2023
+
+@author: DarkPalad1n
+"""
+
+from app import app as application
+
+if __name__ == "__main__":
+    application.run()
